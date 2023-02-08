faf1eb478da27e51d5914efc6962feace8ddcbef
rohitgeddam <sgeddam2@ncsu.edu>
2023-02-08 11:40:05 -0500

    add about controller and view

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'home/index'
+  get 'about', to: "about#index"
+  get 'home', to: "home#index"
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
