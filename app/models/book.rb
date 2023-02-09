00f8dd376e86fd095b4eb96559fba51ad201ee41
varundeepakgudhe <vgudhe@ncsu.edu>
2023-02-09 15:20:24 -0500

    added has many to book and belongs to in review

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,5 @@
 class Book < ApplicationRecord
+  has_many :reviews
+  has_many :transactions
 end
+
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,2 +1,6 @@
+
 class Review < ApplicationRecord
+  belongs_to :book
+  belongs_to :user
 end
+
