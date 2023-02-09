6ffe782e07b793128a46c4f86830cb1e7746eb79
varundeepakgudhe <vgudhe@ncsu.edu>
2023-02-09 16:31:04 -0500

    listing all books at home page

--- config/routes.rb
+++ config/routes.rb
@@ -5,9 +5,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
   get 'about', to: "about#index"
   get 'home', to: "home#index"
-  root 'home#index'
+  root 'books#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  #
   # Defines the root path route ("/")
   # root "articles#index"
 end
