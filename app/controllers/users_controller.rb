1c085beb88c1ee74e78a7d39f6dc889dc0bb0302
rohitgeddam <sgeddam2@ncsu.edu>
2023-02-14 16:41:20 -0500

    add admin panel to manage users

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,59 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user!
+  before_action :is_admin_user?
+
+  def destroy
+    @user = User.find_by(id: params[:id]).destroy
+    redirect_to users_path, notice: "User deleted successfully!"
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  def index
+    @users = User.all
+  end
+
+  def create
+    @user = User.new(user_params)
+    p @user
+    puts " I am herer adfsadf"
+    if @user.save
+      redirect_to users_path, notice: "New user created successfully!"
+    else
+      render :new, notice: "Something went wrong!"
+    end
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    if params[:user][:password].blank? && params[:user][:password_confirmation].blank?
+      params[:user].delete(:password)
+      params[:user].delete(:password_confirmation)
+    end
+    if @user.update(user_params)
+      redirect_to  show_user_path(@user), notice: "User updated successfully!"
+    else
+      render :edit, notice: "Something went wrong!"
+    end
+  end
+
+  private
+  def is_admin_user?
+    if not current_user.admin?
+      redirect_to root_path, alert: "You are not authorized!"
+    end
+  end
+
+  def user_params
+      params.require(:user).permit(:email, :password, :password_confirmation, :username, :name, :address, :phone_number, :credit_card_number)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,21 @@
 Rails.application.routes.draw do
+
   resources :books do
     resources :reviews
   end
   resources :transactions
-  devise_for :users, :controllers => { registrations: 'users/registrations' }
+
+  get 'admin/users', to: "users#index", as: "users"
+  post 'admin/users', to: "users#create", as: "create_users"
+
+  get 'admin/users/new', to: "users#new", as: "new_user"
+
+  get 'admin/users/:id', to: "users#show", as: "show_user"
+  get 'admin/users/:id/edit', to: "users#edit", as: "edit_user"
+  put 'admin/users/:id/update', to: "users#update", as: "update_user"
+  delete 'admin/users/:id', to: "users#destroy", as: "delete_user"
+
   get 'about', to: "about#index"
-  # get 'home', to: "home#index"
+  devise_for :users, :controllers => { registrations: 'users/registrations' }
   root 'books#index'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  #
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
