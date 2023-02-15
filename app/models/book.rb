65e1390081481a2447f854ecf181ebca4d4284f5
rohitgeddam <sgeddam2@ncsu.edu>
2023-02-15 16:39:04 -0500

    add validations to user and review

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,6 +2,13 @@ class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
+
+  validates :name, :presence => true
+  validates :author, :presence => true
+  validates :publisher, :presence => true
+  validates :price, :presence => true
+  validates :stock, :presence => true
+
   def average_rating
     if self.reviews.size > 0
       return self.reviews.average(:rating)
--- app/models/review.rb
+++ app/models/review.rb
@@ -3,5 +3,10 @@ class Review < ApplicationRecord
   belongs_to :book
   belongs_to :user
 
+  validates :book_id, :presence => true
+  validates :user_id, :presence => true
+  validates :rating, :presence => true
+  validates :review, :presence => true
+
 end
 
