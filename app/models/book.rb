c026f9a800cbe6207a87d9206ce49ca38d9a3b31
varundeepakgudhe <vgudhe@ncsu.edu>
2023-02-15 17:38:54 -0500

    book modified validations commited

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,11 +2,11 @@ class Book < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many :transactions, dependent: :destroy
 
-  validates :name, presence: true,length: {minimum: 2, maximum: 100}
-  validates :author, presence: true, length: {minimum: 2, maximum: 100}
-  validates :publisher, presence: true,length: {minimum: 2, maximum: 100}
-  validates :stock, presence: true, numericality: { only_integer: true, greater_than: 0}
-  validates :price, presence: true, numericality: {allow_float: true}
+  validates :name, presence: true,length: {minimum: 1, maximum: 1000}
+  validates :author, presence: true, length: {minimum: 1, maximum: 1000}
+  validates :publisher, presence: true,length: {minimum: 1, maximum: 1000}
+  validates :stock, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0}
+  validates :price, presence: true, numericality: {allow_float: true, greater_than_or_equal_to: 0}
 
 
 
