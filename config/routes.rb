a0657ecce06478c23b9938da34c4e936e04546fa
rohitgeddam <sgeddam2@ncsu.edu>
2023-02-15 11:59:40 -0500

    modified admin delete access

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,7 @@ Rails.application.routes.draw do
 
   get 'admin/users', to: "users#index", as: "users"
   post 'admin/users', to: "users#create", as: "create_users"
-
   get 'admin/users/new', to: "users#new", as: "new_user"
-
   get 'admin/users/:id', to: "users#show", as: "show_user"
   get 'admin/users/:id/edit', to: "users#edit", as: "edit_user"
   put 'admin/users/:id/update', to: "users#update", as: "update_user"
